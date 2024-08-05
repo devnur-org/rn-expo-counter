147651e6882db2c0356c22971ea9e8beb8a07247
Arslan Mushtaq <arslanark@gmail.com>
2024-08-06 03:33:08 +0400

    feat: add simple ui

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.countText}>{count}</Text>
+      <View style={styles.actionWrapper}>
+        <Button title="Decrease" />
+        <Button title="Increase" />
+      </View>
     </View>
   );
 }
@@ -13,8 +18,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    rowGap: 24,
+  },
+  countText: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  actionWrapper: {
+    flexDirection: "row",
+    columnGap: 12,
   },
 });
