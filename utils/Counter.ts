84cc2249a3d03e099db9a129191529be3f7580be
Arslan Mushtaq <arslanark@gmail.com>
2024-08-06 04:09:16 +0400

    feat: create counter class

---
+++ utils/Counter.ts
@@ -0,0 +1,15 @@
+export class Counter {
+  static initialValue: number = 0;
+
+  static increment(prevState: number) {
+    return prevState + 1;
+  }
+
+  static decrement(prevState: number) {
+    return prevState - 1;
+  }
+
+  static reset() {
+    return 0;
+  }
+}
