ef769a532991413a0543d646a7821d8cdf394752
Arslan Mushtaq <arslanark@gmail.com>
2024-08-06 04:18:41 +0400

    docs: add documentation for Counter class

--- utils/Counter.ts
+++ utils/Counter.ts
@@ -1,19 +1,46 @@
+/**
+ * The Counter class provides static methods to manage and manipulate a counter value.
+ */
 export class Counter {
+  /**
+   * The initial value of the counter.
+   * @type {number}
+   * @default 0
+   */
   static initialValue: number = 0;
 
-  static increment(prevState: number) {
+  /**
+   * Increments the given counter value by 1.
+   * @param {number} prevState - The current state of the counter.
+   * @returns {number} The incremented value of the counter.
+   */
+  static increment(prevState: number): number {
     return prevState + 1;
   }
 
-  static decrement(prevState: number) {
+  /**
+   * Decrements the given counter value by 1.
+   * @param {number} prevState - The current state of the counter.
+   * @returns {number} The decremented value of the counter.
+   */
+  static decrement(prevState: number): number {
     return prevState - 1;
   }
 
-  static reset() {
+  /**
+   * Resets the counter value to the initial value (0).
+   * @returns {number} The reset value of the counter (0).
+   */
+  static reset(): number {
     return 0;
   }
 
-  static isZero(count: number) {
+  /**
+   * Checks if the given counter value is zero.
+   * @param {number} count - The value of the counter to check.
+   * @returns {boolean} True if the counter value is zero, otherwise false.
+   */
+  static isZero(count: number): boolean {
     return count === 0;
   }
 }
